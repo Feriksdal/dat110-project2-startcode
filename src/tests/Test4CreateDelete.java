1dea3145b2c1522dcf991a794d4efd44c38fca1d
Erik Fiksdal <erikfix@hotmail.com>
2022-02-23 15:23:10 +0100

    Tester og Intelij-config lagt til

---
+++ src/tests/Test4CreateDelete.java
@@ -0,0 +1,30 @@
+package tests;
+
+import no.hvl.dat110.client.Client;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class Test4CreateDelete extends Test0Base {
+
+	private static String TESTTOPIC = "testtopic";
+	
+	@Test
+	public void test() {
+				
+		Client client = new Client("client",BROKER_TESTHOST,BROKER_TESTPORT);
+		
+		broker.setMaxAccept(1);
+		
+		client.connect();
+		
+		client.createTopic(TESTTOPIC);
+		
+		client.deleteTopic(TESTTOPIC);
+		
+		client.disconnect();
+	
+		assertTrue(true);
+	}
+
+}
