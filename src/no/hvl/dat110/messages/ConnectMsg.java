18ef974d226d7cdcb9b780d49558702b650d31db
h594633 <69585779+h594633@users.noreply.github.com>
2022-02-24 10:18:18 +0100

    git username experiment

--- src/no/hvl/dat110/messages/ConnectMsg.java
+++ src/no/hvl/dat110/messages/ConnectMsg.java
@@ -1,7 +1,7 @@
 package no.hvl.dat110.messages;
 
 public class ConnectMsg extends Message {
-	
+	// test push
 	// message sent by the client when connecting to the broker
 	public ConnectMsg (String user) {
 		super(MessageType.CONNECT, user);
